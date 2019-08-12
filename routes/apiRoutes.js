6ce8a1ef93e1154d61b0bbb208bf5f975a1e4f3b
Rev <treviskaiser@gmail.com>
2019-08-12 18:56:25 -0500

    hide api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,14 @@ const axios = require("axios");
 const router = require("express").Router();
 const RecipeController = require("../controllers/RecipeController.js");
 
+const APIONE = process.env.REACT_APP_COCKTAIL_API_KEY;
+console.log(APIONE);
+
 router.get("/drinks", (req, res) => {
   console.log("HIT DRINK ROUTE");
   const searchText = req.query.q;
   console.log("QUERY TEXT: ", searchText);
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`;
+  const url = `https://www.thecocktaildb.com/api/json/v1/${APIONE}/search.php?s=${searchText}`;
   console.log("URLY", url);
   axios
     .get(url)
